6301143b9e0f6b7ba72c6312bab98f557ad32cf9
aarjavjain1 <42617328+aarjavjain1@users.noreply.github.com>
2018-10-20 11:18:05 +0530

    Update models.py

--- trialprojectpolls/pollsapp/models.py
+++ trialprojectpolls/pollsapp/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
+#This is the questions model for PollsApp
 class Question(models.Model):
 	question_text = models.CharField(max_length = 200)
 	pub_date = models.DateTimeField('date published')
 	def __str__(self):
 		return self.question_text
 
+#This is the Choice model for PollsApp
 class Choice(models.Model):
 	quesion = models.ForeignKey(Question, on_delete = models.CASCADE)
 	choice_text = models.CharField(max_length = 200)
 	votes = models.IntegerField(default = 0)
 	def __str__(self):
 		return self.choice_text
-
