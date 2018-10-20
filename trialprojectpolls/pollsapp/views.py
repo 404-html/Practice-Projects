455c21d395fe9a738b0c55cb3a971367520d08ea
aarjavjain1 <42617328+aarjavjain1@users.noreply.github.com>
2018-10-20 11:20:31 +0530

    Update views.py

--- trialprojectpolls/pollsapp/views.py
+++ trialprojectpolls/pollsapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def index(request): #This is the home page of the Website
 	return HttpResponse("<h1>Hello World. You are at the polls index.</h1>")
