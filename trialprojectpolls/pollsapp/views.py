853aaa65a0f56c80435253edc457c2a8718e2bfc
Aarjav Jain <aarjav00@gmail.com>
2018-10-21 02:11:25 +0530

    Completed half of Part 4

--- trialprojectpolls/pollsapp/views.py
+++ trialprojectpolls/pollsapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from .models import Choice, Question
+from django.urls import reverse
 
 def index(request): #This is the home page of the Website
 	latest_questions_list = Question.objects.order_by('-pub_date')[ :5]
@@ -16,9 +17,18 @@ def detail(request, question_id):
 	return render (request, 'pollsapp/detail.html', {'question': question})
 
 def results(request, question_id):
-	response = "You are looking at the results of question %s"
-	return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'pollsapp/results.html', {'question': question})
 
 def vote(request, question_id):
-	return HttpResponse("You are voting on question %s" % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		#Redisplay the question voting form.
+		return render( request, 'pollsapp/detail.html'), {'question':question, 'error_message': "You did not select a choice"}
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('pollsapp:results', args=(question.id,)))
 
